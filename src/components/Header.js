96b67677fd88a1d04036c0f7d50a617223950a01
Alex Zheng <zhengzhouyang3@gmail.com>
2022-06-11 15:02:04 -0500

    fake shop demo init

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Fake Shop Demo</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
